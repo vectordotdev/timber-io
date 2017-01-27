de0a0f1c26a63fe52f824450737f2c5f8ad23a47
Zach Sherman <zksherm@gmail.com>
2017-01-27 15:42:58 -0500

    some carousel fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 $(function () {
+
+  var highlightActiveButton = function(el) {
+    // Remove other dark classes
+    $(el).parent().find('.bg-dark-purple').removeClass('bg-dark-purple').addClass('bg-paua');
+
+    // Make this button active
+    $(el).addClass('bg-dark-purple');
+
+    // Remove active state from number circle
+    $(el).parent().find('.bg-purple').removeClass('bg-purple').addClass('bg-midnight');
+
+    // Make this button's circle active
+    $(el).find('.bg-midnight').removeClass('bg-midnight').addClass('bg-purple');
+  }
   
-  const $demoCarousel = $(".home__demo__carousel__content").slick({
+  // Initialize the carousel
+  var $demoCarousel = $(".home__demo__carousel__content").slick({
     infinite: true,
     slidesToShow: 1,
     centerMode: true,
@@ -10,12 +25,22 @@ $(function () {
     prevArrow: ""
   });
 
+  // Highlight selected carousel button when it changes
+  $demoCarousel.on('afterChange', function(event, slick, currentSlide) {
+    var buttonToHighlight = $('.home__demo__carousel__nav button').eq(currentSlide)[0];
+    highlightActiveButton(buttonToHighlight);
+  })
+
+  // Handle explicit carousel selection
   $('.home__demo__carousel__nav button').on('click', function() {
     $demoCarousel.slick('slickGoTo', $(this).index());
-    $(this).parent().find('.bg-dark-purple').removeClass('bg-dark-purple');
-    $(this).addClass('bg-dark-purple');
-    $(this).find('span').first().addClass('bg-purple');
+    highlightActiveButton(this);
   });
+
+  // Make the first button active
+  $('.home__demo__carousel__nav button').first().click();
+
+  
 });
 
 /* Mobile menu */
