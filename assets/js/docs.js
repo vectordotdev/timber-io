f71f53e075e8a0e206a5daba4b1bdc746f676815
Ben Johnson <bjohnson@binarylogic.com>
2017-03-19 09:44:08 -0500

    Add header link icon

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -10,7 +10,7 @@ $(function () {
     // });
 
     $('.markdown-body h2,h3,h4,h5,h6').filter('[id]').each(function () {
-      $(this).html('<a href="#'+$(this).attr('id')+'">' + $(this).text() + '</a>');
+      $(this).html('<a href="#'+$(this).attr('id')+'" class="heading-anchor"><i class="fa fa-link" aria-hidden="true"></i>' + $(this).text() + '</a>');
     });
 
     $('a.chat').on('click', function() {
